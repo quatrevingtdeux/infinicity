6ecc9be7d2c621b744b6be4768d8f619ba0fcf90
Nicolas Useldinger <quatrevingtdeux@gmail.com>
2011-05-03 19:55:38 +0200

    BUG : certains carrés sortent

--- city.cpp
+++ city.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 #include <time.h>
 #include <iostream>
+#include <math.h>
+
+// > 1.f
+#define SCALE_DIAMETRE 2.f
+// < 1.f
+#define POURCENTAGE_DISTANCE 0.f
 
 // -BLOCK----------------------------------------------------------------------
 /*!
@@ -105,100 +111,46 @@ Vector gimmeAPoint(const Vector& a, const Vector& b)
 	return Vector(x,y,z);
 }
 
+void DonnerPoint(Vector& point, const Vector& a, const Vector& b, double pourcentage)
+{
+	double rayon;
+	point = a + (b-a)/2.f;
+	rayon = abs(pourcentage/2 * (a[0]-b[0]));
+	point[0] = point[0] + rand()%(int)(2*rayon+1) - rayon;
+	rayon = abs(pourcentage/2 * (a[1]-b[1]));
+	point[1] = point[1] + rand()%(int)(2*rayon+1) - rayon;
+	rayon = abs(pourcentage/2 * (a[2]-b[2]));
+	point[2] = point[2] + rand()%(int)(2*rayon+1) - rayon;
+}
+
 /*
 \brief Subdivide a quadrangular land parcel. 
 */
 void LandQuad::Subdivide()
 {
-// 	// Area
-// 	double area=Area();
-// 
-// 	// Check if minimal area : in this case stop 
-// 	// land use subdivision and start base building generation
-// 	if (area<30)  
-// 	{
-// 		//Quadrangle q = Quadrangle(p[0], p[1], p[2], p[3])
-// 		//						.Shrink(0.25, 0.25, 0.25, 0.25);
-// 		static double sh=0.25;
-// 		//sh+=0.001;
-// 		Quadrangle q = Quadrangle(p[0], p[1], p[2], p[3]).Shrink(sh,sh,sh,sh);
-// 		//PrismQuad(q, 0.5*(1+rand()%5)).Render();
-// 		PrismQuad(q, 5).Render(); 
-// 	}
-// 	// Land use subdivison
-// 	else
-// 	{
-// 		/*srand(time(NULL));
-// 		if (area > 30)
-// 		{
-// 			Vector real_center(p[0][0]+(p[2][0]-p[0][0])/2,
-// 						    p[0][1]+(p[2][1]-p[0][1])/2,0);
-// 			
-// 			double minx = p[0][0], maxx = p[2][0];
-// 			double miny = p[0][1], maxy = p[2][1];
-// 			double largeur = maxx -minx;
-// 			double thres = 0.1*largeur;
-// 			double rand_x = minx + thres + (rand()%(int)(largeur-2*thres));
-// 			double rand_y = miny + thres + (rand()%(int)(largeur-2*thres));
-// 			/// TODO : passer en valeurs flottantes !
-// 			
-// 			Vector city_center(rand_x,rand_y,0);
-// 			
-// 			
-// 			std::cout << thres << std::endl;
-// 			std::cout << city_center[0] << " " << city_center[1] << std::endl;
-// 			std::vector<Vector> points;
-// 			
-// 			for (int i = 0; i < 4; ++i)
-// 			{
-// 				int nb_points = rand()%3;
-// 				points.push_back(p[i%4]);
-// 				while (nb_points-- > 0)
-// 				{
-// 					std::cout << (i+1)%4 << " " << p[i%4][0] << std::endl;
-// 					points.push_back(gimmeAPoint(p[i%4],p[(i+1)%4]));
-// 				}
-// 				points.push_back(p[(i+1)%4]);
-// 			}
-// 			
-// 			
-// 			
-// 			std::vector<Vector>::iterator ite, ite2 = points.begin();
-// 			ite2++;
-// 			for (ite = points.begin(); ite != points.end(); ++ite)
-// 			{
-// 				if (ite2 == points.end())
-// 					ite = points.begin();
-// 				std::cout << (*ite)[0] << " " << (*ite)[1] << std::endl;
-// 				LandQuad(*ite, *ite2, real_center, real_center)
-// 								.Subdivide();
-// 				ite2++;
-// 			}
-// 			
-// 		}*/
-
-// 	}
+	static double height = 1;
+	//	height+=0.1;
 	double area=Area();
-	std::cout << "area:" << area << std::endl;
+	//std::cout << "area:" << area << std::endl;
 
 	//if (area<10)
-	//if (area<800) // multicentre
-	if (area < 3200) // un centre
+	//if (area<100) // multicentre
+	if (area < 100) // un centre
 	{
+		//std::cout << "-- Rendu -- " << area << std::endl;
 		Quadrangle q=Quadrangle(p[0],p[1],p[2],p[3]).Shrink(0.25,0.25,0.25,0.25);
 		//PrismQuad(q,0.5*(1+rand()%5)).Render(); 
-		PrismQuad(q,5).Render(); 
+		PrismQuad(q,height).Render(); 
 	}
 	else
 	{
-// 		LandTriangle(p[0],p[1],p[2]).Subdivide();
-// 		LandTriangle(p[0],p[2],p[3]).Subdivide();
-		srand(time(NULL));
-		double aleatoire = 0;
-		for (int i = 0; i < 4; i++)
-			aleatoire = area*(rand()%100-50)/100000;
 
-		Vector mid   = p[0]+(p[2]-p[0])/(2+aleatoire);
+		Vector mid   = p[0]+(p[2]-p[0])/2;
+		double diametre;
+		diametre = (p[1][0]-p[0][0])/SCALE_DIAMETRE;
+		mid[0] += rand()%(int)diametre - diametre/2.f;
+		diametre = (p[2][1]-p[1][1])/SCALE_DIAMETRE;
+		mid[1] += rand()%(int)diametre - diametre/2.f;
 		
 		// pour avoir les milieux des bords
 		Vector mid01 = p[0]+(p[1]-p[0])/2;
@@ -210,42 +162,46 @@ void LandQuad::Subdivide()
 		Vector mid03 = p[0]+(p[3]-p[0])/2;
 		//mid03[1] = mid[1];
 		
-		// pour avoir des avenues rand
-		double aleatmp = rand();
-		aleatmp = (aleatmp+rand())/aleatmp;
-		aleatmp -= (int) aleatmp;
-		aleatmp = (aleatmp < 0.2f) ? aleatmp+0.2:aleatmp;
-		aleatmp = (aleatmp > 0.8f) ? aleatmp-0.2:aleatmp;
-		std::cout << aleatmp << std::endl;
-		mid01[0] = p[0][0] + aleatmp*(p[1][0]-p[0][0]);
-		//mid01[1] = mid[1];
+		DonnerPoint(mid01, p[0], p[1], POURCENTAGE_DISTANCE);
+		DonnerPoint(mid12, p[1], p[2], POURCENTAGE_DISTANCE);
+		DonnerPoint(mid23, p[2], p[3], POURCENTAGE_DISTANCE);
+		DonnerPoint(mid03, p[0], p[3], POURCENTAGE_DISTANCE);
 		
+		if (-20.0 > p[0][0] || p[0][0] > 20.0
+			|| -20.0 > p[0][1] || p[0][1] > 20.0)
+			std::cout << "WARNING ! p[0] : x" << p[0][0] << " y" << p[0][1] << std::endl;
 		
-		aleatmp = rand();
-		aleatmp = (aleatmp+rand())/aleatmp;
-		aleatmp -= (int) aleatmp;
-		aleatmp = (aleatmp < 0.2f) ? aleatmp+0.2:aleatmp;
-		aleatmp = (aleatmp > 0.8f) ? aleatmp-0.2:aleatmp;
-		std::cout << aleatmp << std::endl;
-		mid12[1] = p[1][1] + aleatmp*(p[2][1]-p[1][1]);
+		if (-20.0 > p[1][0] || p[1][0] > 20.0
+			|| -20.0 > p[1][1] || p[1][1] > 20.0)
+			std::cout << "WARNING ! p[1] : x" << p[1][0] << " y" << p[1][1] << std::endl;
 		
+		if (-20.0 > p[2][0] || p[2][0] > 20.0
+			|| -20.0 > p[2][1] || p[2][1] > 20.0)
+			std::cout << "WARNING ! p[2] : x" << p[2][0] << " y" << p[2][1] << std::endl;
 		
-		aleatmp = rand();
-		aleatmp = (aleatmp+rand())/aleatmp;
-		aleatmp -= (int) aleatmp;
-		aleatmp = (aleatmp < 0.2f) ? aleatmp+0.2:aleatmp;
-		aleatmp = (aleatmp > 0.8f) ? aleatmp-0.2:aleatmp;
-		std::cout << aleatmp << std::endl;
-		mid23[0] = p[2][0] + aleatmp*(p[3][0]-p[2][0]);
+		if (-20.0 > p[3][0] || p[3][0] > 20.0
+			|| -20.0 > p[3][1] || p[3][1] > 20.0)
+			std::cout << "WARNING ! p[3] : x" << p[3][0] << " y" << p[3][1] << std::endl;
 		
+		if (-20.0 > mid[0] || mid[0] > 20.0
+			|| -20.0 > mid[1] || mid[1] > 20.0)
+			std::cout << "WARNING ! mid : x" << mid[0] << " y" << mid[1] << std::endl;
 		
-		aleatmp = rand();
-		aleatmp = (aleatmp+rand())/aleatmp;
-		aleatmp -= (int) aleatmp;
-		aleatmp = (aleatmp < 0.2f) ? aleatmp+0.2:aleatmp;
-		aleatmp = (aleatmp > 0.8f) ? aleatmp-0.2:aleatmp;
-		std::cout << aleatmp << std::endl;
-		mid03[1] = p[0][1] + aleatmp*(p[3][1]-p[0][1]);
+		if (-20.0 > mid01[0] || mid01[0] > 20.0
+			|| -20.0 > mid01[1] || mid01[1] > 20.0)
+			std::cout << "WARNING ! mid01 : x" << mid01[0] << " y" << mid01[1] << std::endl;
+				
+		if (-20.0 > mid12[0] || mid12[0] > 20.0
+			|| -20.0 > mid12[1] || mid12[1] > 20.0)
+			std::cout << "WARNING ! mid12 : x" << mid12[0] << " y" << mid12[1] << std::endl;
+				
+		if (-20.0 > mid23[0] || mid23[0] > 20.0
+			|| -20.0 > mid23[1] || mid23[1] > 20.0)
+			std::cout << "WARNING ! mid23 : x" << mid23[0] << " y" << mid23[1] << std::endl;
+				
+		if (-20.0 > mid03[0] || mid03[0] > 20.0
+			|| -20.0 > mid03[1] || mid03[1] > 20.0)
+			std::cout << "WARNING ! mid03 : x" << mid03[0] << " y" << mid03[1] << std::endl;
 		
 		
 		LandQuad(p[0], mid01, mid, mid03).Subdivide();
