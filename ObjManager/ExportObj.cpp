aa95efcf51c4d3af990cdd36005cf5fb96987dd4
Nicolas Useldinger <quatrevingtdeux@gmail.com>
2011-05-31 19:56:55 +0200

    now allow use of triangular faces

--- ObjManager/ExportObj.cpp
+++ ObjManager/ExportObj.cpp
@@ -57,24 +57,36 @@ void ExportObj::ExportFaces(std::vector<Face*>* faces)
 	}
 	
 	// writing faces
-	int next_id = 1;
-	int id1, id2, id3;
+	int next_id = 1, size;
+	int id, id1, id2, id3;
 	
+	// so dirty
 	for (iteFace = faces->begin(); iteFace != faces->end(); ++iteFace)
 	{
-		id1 = next_id;
-		id2 = next_id + 1;
-		id3 = next_id + 3;
-		file << "f  " << id1 << "//  " << id2 << "//  ";
-		file << id3 << "// " << std::endl;
+		size = (*iteFace)->GetVertices().size();
+		if (size == 4)
+		{
+			id1 = next_id;
+			id2 = next_id + 1;
+			id3 = next_id + 3;
+			file << "f  " << id1 << "//  " << id2 << "//  ";
+			file << id3 << "// " << std::endl;
+			
+			id1 = next_id;
+			id2 = next_id + 2;
+			id3 = next_id + 3;
+			file << "f  " << id1 << "//  " << id2 << "//  ";
+			file << id3 << "// " << std::endl;
 		
-		id1 = next_id;
-		id2 = next_id + 2;
-		id3 = next_id + 3;
-		file << "f  " << id1 << "//  " << id2 << "//  ";
-		file << id3 << "// " << std::endl;
-		
-		next_id += 4;
+			next_id += 4;
+		}
+		else if (size ==  3)
+		{
+			id = next_id;
+			file << "f  " << id << "//  " << id+1 << "//  ";
+			file << id+2 << "// " << std::endl;
+			next_id += 3;
+		}
 	}
 }
 
