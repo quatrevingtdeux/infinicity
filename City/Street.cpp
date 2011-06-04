c8bc6221744067642d01f0ba7e0a3823148d2afa
Nicolas Useldinger <quatrevingtdeux@gmail.com>
2011-06-04 15:20:12 +0200

    cleaning 4 edges street / New 3 edges street

--- City/Street.cpp
+++ City/Street.cpp
@@ -3,6 +3,7 @@
 #include "Street.h"
 #include "../ObjManager/ImportObj.h"
 #include "../define.h"
+#include "../Geom/GeomOp.h"
 
 Street::Street()
 {
@@ -23,70 +24,26 @@ Street::~Street()
 
 void Street::Build()
 {
-	CreatePlaneStreet();
+	if (vertices->size() == 4)
+		CreatePlaneStreet();
+	else if (vertices->size() == 3)
+		CreateTriangleStreet();
 }
 
 void Street::CreatePlaneStreet()
 {
-	// TODO lol refaire
 	double height = 0.2f;
-	Vertex *v000 = new Vertex(vertices->at(0)->X(), vertices->at(0)->Y(), 0.f);
-	Vertex *v100 = new Vertex(vertices->at(1)->X(), vertices->at(1)->Y(), 0.f);
-	Vertex *v010 = new Vertex(vertices->at(3)->X(), vertices->at(3)->Y(), 0.f);
-	Vertex *v110 = new Vertex(vertices->at(2)->X(), vertices->at(2)->Y(), 0.f);
-	Vertex *v001 = new Vertex(vertices->at(0)->X(), vertices->at(0)->Y(), height);
-	Vertex *v101 = new Vertex(vertices->at(1)->X(), vertices->at(1)->Y(), height);
-	Vertex *v011 = new Vertex(vertices->at(3)->X(), vertices->at(3)->Y(), height);
-	Vertex *v111 = new Vertex(vertices->at(2)->X(), vertices->at(2)->Y(), height);
-	std::vector<Vertex*>* tempFaceVert = new std::vector<Vertex*>();
+	std::vector<Vertex *> vert;
+	for (int i = 0; i < 4; i++)
+	{
+		// TODO lol refaire
+		// anti bug trick
+		int u = (i == 2) ? 3 : ((i == 3) ? 2 : i);
+		Vertex *tmp = vertices->at(u);
+		vert.push_back(new Vertex(tmp->X(), tmp->Y(), height));
+	}
 	
-	//bottom
-	tempFaceVert->clear();
-	tempFaceVert->push_back(v000);
-	tempFaceVert->push_back(v010);
-	tempFaceVert->push_back(v100);
-	tempFaceVert->push_back(v110);
-	faces->push_back(new Face(new std::vector<Vertex*>(*tempFaceVert)));
-	
-	// up
-	tempFaceVert->clear();
-	tempFaceVert->push_back(v001);
-	tempFaceVert->push_back(v011);
-	tempFaceVert->push_back(v101);
-	tempFaceVert->push_back(v111);
-	faces->push_back(new Face(new std::vector<Vertex*>(*tempFaceVert)));
-	
-	// front
-	tempFaceVert->clear();
-	tempFaceVert->push_back(v000);
-	tempFaceVert->push_back(v010);
-	tempFaceVert->push_back(v001);
-	tempFaceVert->push_back(v011);
-	faces->push_back(new Face(new std::vector<Vertex*>(*tempFaceVert)));
-			
-	// back
-	tempFaceVert->clear();
-	tempFaceVert->push_back(v100);
-	tempFaceVert->push_back(v110);
-	tempFaceVert->push_back(v101);
-	tempFaceVert->push_back(v111);
-	faces->push_back(new Face(new std::vector<Vertex*>(*tempFaceVert)));
-	
-	// left
-	tempFaceVert->clear();
-	tempFaceVert->push_back(v000);
-	tempFaceVert->push_back(v100);
-	tempFaceVert->push_back(v001);
-	tempFaceVert->push_back(v101);
-	faces->push_back(new Face(new std::vector<Vertex*>(*tempFaceVert)));
-	
-	// right
-	tempFaceVert->clear();
-	tempFaceVert->push_back(v010);
-	tempFaceVert->push_back(v110);
-	tempFaceVert->push_back(v011);
-	tempFaceVert->push_back(v111);
-	faces->push_back(new Face(new std::vector<Vertex*>(*tempFaceVert)));
+	faces->push_back(new Face(new std::vector<Vertex *>(vert)));
 	
 	// TODO ajouter trottoir
 	
@@ -102,4 +59,20 @@ void Street::CreatePlaneStreet()
 		(*itf)->Translate(v->X(), v->Y(), v->Z());
 		faces->push_back((*itf));	
 	}
+}
+
+void Street::CreateTriangleStreet()
+{
+	assert(vertices->size() == 3);
+	
+	std::cout << "triangle street" << std::endl;
+	double height = 0.2f;
+	std::vector<Vertex *> vert;
+	for (int i = 0; i < 3; i++)
+	{
+		Vertex *tmp = vertices->at(i);
+		vert.push_back(new Vertex(tmp->X(), tmp->Y(), height));
+	}
+	
+	faces->push_back(new Face(new std::vector<Vertex *>(vert)));
 }
